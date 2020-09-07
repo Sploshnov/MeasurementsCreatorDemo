3189e8e3fd0ae785b2453fba9375345b19175e43
Sploshnov <kostiantyn.sploshnov@rif-ev.de>
2020-09-07 14:18:30 +0200

    added ui manipulators for camera pos and view point

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { THREEService } from "./services/THREEService";
 import { GripperPartSettings } from "./models/GripperPartSettings";
+import { Vector3 } from "three";
 
 module.exports = {
     THREEService: THREEService,
-    GripperPartSettings: GripperPartSettings
+    GripperPartSettings: GripperPartSettings,
+    Vector3: Vector3
 }
